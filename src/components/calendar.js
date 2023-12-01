5fe220905f546626f3441b0c192071e8ac6aa04f
AlexIvar <alexivar65@gmail.com>
2023-12-01 01:50:49 +0000

    hello world

---
+++ src/components/calendar.js
@@ -0,0 +1,160 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import CalendarItem from './calendarItem';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import moment from"moment";
+
+console.log(moment().date())
+console.log( moment().date() + 1)
+
+let items = [
+    {
+       "id": 1,
+       "text": "Happy holidays baltasito",
+       "color": moment().date() >= 1 ?  "#2979ff" : "#1c54b2",
+       "title": "Enchanted to meet you",
+       "body": " Today marks the first of December, signifying a full month since we started talking. Each passing day with you has been a treasure, filling my heart with joy and gratitude.",
+       "body2": " You've become an irreplaceable part of my world, making each moment brighter and more beautiful. I feel incredibly lucky to have crossed paths with someone as amazing as you. Your laughter, your kindness, and your presence bring an indescribable warmth to my life. ",
+       "body3": "I cherish every conversation, every shared smile, and every memory we've created together in this short time. As we celebrate this small milestone, know that my affection for you grows with each passing day. Your presence in my life is a gift I cherish, and I'm eagerly looking forward to the days ahead filled with more laughter, adventures, and shared moments. I love you so so much my sweet Baltasita.",
+       "body4": "Thank you for being the wonderful person you are."
+    },
+    {
+        "id": 2,
+        "text": "I love youu",
+        "color": moment().date() >= 2 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 3,
+        "text": "We are family",
+        "color": moment().date() >= 3 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 4,
+        "text": "I am so glad to have met you",
+        "color": moment().date() >= 4 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 5,
+        "text": "Happy holidays",
+        "color": moment().date() >= 5 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+         "id": 6,
+         "text": "Happy holidays",
+         "color": moment().date() >= 6 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 7,
+        "text": "Happy holidays",
+        "color": moment().date() >= 7 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+         "id": 8,
+         "text": "Happy holidays",
+         "color": moment().date() >= 8 ?  "#2979ff" : "#1c54b2",
+    },
+        {
+        "id": 9,
+        "text": "Happy holidays",
+        "color": moment().date() >= 9 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+         "id": 10,
+         "text": "Happy holidays",
+         "color": moment().date() >= 10 ?  "#2979ff" : "#1c54b2",
+    },
+    ,
+        {
+        "id": 11,
+        "text": "Happy holidays",
+        "color": moment().date() >= 11 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 12,
+        "text": "Happy holidays",
+        "color": moment().date() >= 12 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 13,
+        "text": "Happy holidays",
+        "color": moment().date() >= 13 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 14,
+        "text": "Happy holidays",
+        "color": moment().date() >= 14 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 15,
+        "text": "Happy holidays",
+        "color": moment().date() >= 15 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 16,
+        "text": "Happy holidays",
+        "color": moment().date() >= 16 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 17,
+        "text": "Happy holidays",
+        "color": moment().date() >= 17 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 18,
+        "text": "Happy holidays",
+        "color": moment().date() >= 18 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 19,
+        "text": "Happy holidays",
+        "color": moment().date() >= 19 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 20,
+        "text": "Happy holidays",
+        "color": moment().date() >= 20 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 21,
+        "text": "Happy holidays",
+        "color": moment().date() >= 21 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 22,
+        "text": "Happy holidays",
+        "color": moment().date() >= 22 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 23,
+        "text": "Happy holidays",
+        "color": moment().date() >= 23 ?  "#2979ff" : "#1c54b2",
+    },
+    {
+        "id": 24,
+        "text": "Happy holidays",
+        "color": moment().date() >= 24 ?  "#2979ff" : "#1c54b2",
+    },
+  ]
+
+export const Calendar = () => {
+    return (
+        <div>
+            <Typography gutterBottom variant="h4" component="div">
+            Mafesita's xmas calendar
+            </Typography>
+           
+            <Grid container spacing={{ xs: 1, sm: 2, md: 2, lg: 5, xl: 3 }} columns={{ xs: 10, sm: 10, md: 10, lg: 10, xl: 4 }}  sx={{padding: 5}}>
+                {items.map((item) => {
+                    return (
+                        <Grid item xs={4} md={4} lg={2} xl={4} key={item.id}>
+                            <CalendarItem numberid={item.id} cardText={item.text} cardColor={item.color} cardTitle={item.title} cardBody={item.body} cardBody2={item.body2} cardBody3={item.body3} cardBody4={item.body4}/>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </div>
+    )
+}
+export default Calendar;
---
+++ src/components/calendarItem.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ParkIcon from '@mui/icons-material/Park';
+import ForestIcon from '@mui/icons-material/Forest';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import StarIcon from '@mui/icons-material/Star';
+import CookieIcon from '@mui/icons-material/Cookie';
+import CelebrationIcon from '@mui/icons-material/Celebration';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import CakeIcon from '@mui/icons-material/Cake';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FilterVintageIcon from '@mui/icons-material/FilterVintage';
+import PetsIcon from '@mui/icons-material/Pets';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import YardIcon from '@mui/icons-material/Yard';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import WcIcon from '@mui/icons-material/Wc';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MusicVideoIcon from '@mui/icons-material/MusicVideo';
+import LooksIcon from '@mui/icons-material/Looks';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import IcecreamIcon from '@mui/icons-material/Icecream';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import moment from"moment";
+import Grid from '@mui/material/Grid';
+
+const Item2 = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }));
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+    lineHeight: '60px',
+    backgroundColor: 'red',
+    variant: "outlined",
+    color: "white"
+  }));
+
+
+export const CalendarItem = (props) => {
+    
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = (props) => {
+        if(moment().date() >= props.numberid){
+            setOpen(true);
+          }
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    console.log(props)
+  return (
+    <>
+    <Item onClick={() => handleClickOpen(props)} elevation={3}>
+   
+    <Card sx={{background: props.cardColor}}>
+    <Typography gutterBottom variant="h4" component="div" sx={{padding: 2}}>
+          {props.numberid}
+        </Typography>
+      <CardContent sx={{textAlign: "center"}}>
+       {props.numberid === 1 ? <FavoriteIcon sx={{color: "white", width: 40, height: 50}}/> : 
+        props.numberid === 2 ? <ForestIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 3 ? <NightlightIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 4 ? <StarIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 5 ? <CookieIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 6 ? <CelebrationIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 7 ? <AcUnitIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 8 ? <CakeIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 9 ? <AutoAwesomeIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 10 ? <AudiotrackIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 11 ? <FavoriteBorderIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 12 ? <FilterVintageIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 13 ? <PetsIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 14 ? <ScheduleIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 15 ? <StarBorderIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 16 ? <YardIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 17 ? <WbSunnyIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 18 ? <WcIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 19 ? <NotificationsIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 20 ? <MusicVideoIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 21 ? <LooksIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 22 ? <LightbulbIcon sx={{color: "white",width: 40, height: 50}}/> :
+        props.numberid === 23 ? <LocalPizzaIcon sx={{color: "white",width: 40, height: 50}}/> :
+                               <IcecreamIcon sx={{color: "white",width: 40, height: 50}}/>} 
+      </CardContent>
+    </Card>
+    </Item>
+     <BootstrapDialog
+       onClose={handleClose}
+       aria-labelledby="customized-dialog-title"
+       open={open}
+     >
+       <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+       {props.cardTitle}
+       </DialogTitle>
+       <IconButton
+         aria-label="close"
+         onClick={handleClose}
+         sx={{
+           position: 'absolute',
+           right: 8,
+           top: 8,
+           color: (theme) => theme.palette.grey[500],
+         }}
+       >
+         <CloseIcon />
+       </IconButton>
+       <DialogContent dividers>
+         <Typography gutterBottom>
+         {props.cardBody}
+         </Typography>
+         <Typography gutterBottom>
+         {props.cardBody2}
+         </Typography>
+         <Typography gutterBottom>
+         {props.cardBody3}
+         </Typography>
+         <Typography gutterBottom>
+         {props.cardBody4}
+         </Typography>
+       </DialogContent>
+       <DialogActions>
+       </DialogActions>
+     </BootstrapDialog>
+     </>
+  )
+}
+
+export default CalendarItem;
